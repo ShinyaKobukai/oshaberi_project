d22e76116d99a885766a7b3d6ce8c71ab53aaa07
ShinyaKobukai <kobukai.0415@gmail.com>
2020-01-15 11:23:20 +0900

    first commit

---
+++ js/chain_method.js
@@ -0,0 +1,4 @@
+// $(body).append('<audio src="あ.wav' />').append(<audio src="い.wav" />')....
+// $("body").append('<audio src="word/'.$ar_word[$i].'.wav" autoplay></audio>');
+// <audio src="word/'.$ar_word[$i].'.wav" autoplay></audio>
+$("body").append('<audio src="word/'.$ar_word[$i].'.wav" autoplay></audio>');
